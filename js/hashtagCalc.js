95577841a6d297541befb398306a58db309971bb
pereznetworks <34382720+pereznetworks@users.noreply.github.com>
2022-06-30 10:12:51 -0700

    code cleanup

--- js/hashtagCalc.js
+++ js/hashtagCalc.js
@@ -18,109 +18,89 @@ let opPressed = false;
 
 // evaluate current input, operators, numbers, complete math operation
 const calcEval = {
-
 	ops: function () {
-
 		if ( opValue === '' ) {  // this is the very first operator entered
-
 			firstNumberValue = parseFloat(numberValue, 10);
 			debug.log('firstNumberValue: ' + firstNumberValue);
 			calcSum = calc.addition(firstNumberValue, secondNumberValue);
 			ux.displayValue(calcSum);
 			debug.log("calcSum:" + calcSum);
-
 		} else if ( opValue === "+" || opValue === "-" || opValue === "*" || opValue === "/" ) {  // entered something like, + 3 = or 3 + 4 =
-
 			secondNumberValue = parseFloat(numberValue, 10);
 			debug.log('secondNumberValue: ' + secondNumberValue);
 			debug.log('opValue: ' + opValue);
-
 		} else if ( calcStatus >= 2 ) {  // entered something like, 3 + 3 = 6 + 4 =
-
 			firstNumberValue = calcSum;
 			debug.log('firstNumberValue: ' + firstNumberValue);
 			secondNumberValue = parseFloat(numberValue, 10);
 			debug.log('secondNumberValue: ' + secondNumberValue);
 			debug.log('opValue: ' + opValue);
-
 		}
 
 	  // do math, get and save calcSum
-
 		if ( opValue === '/' ) {
-
 				calcSum = calc.divide(firstNumberValue, secondNumberValue);
 				ux.displayValue(calcSum);
 				debug.log("calcSum:" + calcSum);
-
 		} else if ( opValue === '*' ) {
-
 				calcSum = calc.multiply(firstNumberValue, secondNumberValue);
 				ux.displayValue(calcSum);
 				debug.log("calcSum:" + calcSum);
-
 		} else if ( opValue === '-' ) {
-
 				calcSum = calc.subtract(firstNumberValue, secondNumberValue);
 				ux.displayValue(calcSum);
 				debug.log("calcSum:" + calcSum);
-
 		} else if ( opValue === '+' ) {
-
 				calcSum = calc.addition(firstNumberValue, secondNumberValue);
 				ux.displayValue(calcSum);
 				debug.log("calcSum:" + calcSum);
-
 		}
-
 	}
 }
 
 // math happens here
 const calc = {
-
-		sumValue: 0,
-		allClear: function() {
-			this.sumValue = 0;
+	sumValue: 0,
+	allClear: function() {
+		this.sumValue = 0;
+		return this.sumValue;
+	},
+	divide: function(numValue1, numValue2) {
+		// normal divide
+		this.sumValue = numValue1 / numValue2;
+		return this.sumValue;
+	},
+	multiply: function(numValue1, numValue2) {
+		// normal mutliply
+		this.sumValue = numValue1 * numValue2;
+		return this.sumValue;
+	},
+	subtract: function(numValue1, numValue2) {
+		// normal subtract
+		this.sumValue = numValue1 - numValue2;
+		return this.sumValue;
+	},
+	addition: function(numValue1, numValue2) {
+		// normal addition
+		this.sumValue = numValue1 + numValue2;
+		return this.sumValue;
+	},
+	percent: function(numberOfValues, numValue1, numValue2){
+		if (numberOfValues == 0 ) {
+			this.sumValue = numValue1 / 100;
 			return this.sumValue;
-		},
-		divide: function(numValue1, numValue2) {
-			// normal divide
-			this.sumValue = numValue1 / numValue2;
-			return this.sumValue;
-		},
-		multiply: function(numValue1, numValue2) {
-			// normal mutliply
-			this.sumValue = numValue1 * numValue2;
-			return this.sumValue;
-		},
-		subtract: function(numValue1, numValue2) {
-			// normal subtract
-			this.sumValue = numValue1 - numValue2;
-			return this.sumValue;
-		},
-		addition: function(numValue1, numValue2) {
-			// normal addition
-			this.sumValue = numValue1 + numValue2;
-			return this.sumValue;
-		},
-		percent: function(numberOfValues, numValue1, numValue2){
-
-			if (numberOfValues == 0 ) {
-				this.sumValue = numValue1 / 100;
-				return this.sumValue;
-			} else if ( numberOfValues == 1 ) {
-				this.sumValue = numValue1 * ( numValue2 / 100 )
-				return this.sumValue;
-			}
-		},
-		posNeg: function(numValue1){
-			this.sumValue = numValue1 * -1;
-			return this.sumValue;
-		},
-		equals: function() {
+		} else if ( numberOfValues == 1 ) {
+			this.sumValue = numValue1 * ( numValue2 / 100 )
 			return this.sumValue;
 		}
+	},
+	posNeg: function(numValue1){
+		this.sumValue = numValue1 * -1;
+		return this.sumValue;
+	},
+	equals: function() {
+		return this.sumValue;
+	}
 }
 
 
@@ -131,40 +111,39 @@ const tape = {
 
 const debug = {
 // to be replaced by logging state of app and errors
-
 	loggingOn: false,
-
 	log: function (value){
 		if(this.loggingOn){
 			this.log = console.log.bind(window.console);
 		}
 	}
-
 }
 
 // UI specific tasks
 const ux = {
-
 	acShow: true,  // whether to show AC
 
 	acToogle: function () {
 
-				debug.log('run acToogle:' + this.acShow);
+		debug.log('run acToogle:' + this.acShow);
 
-				if ( this.acShow ) {
-					// then toogle C button to AC
-					$('#ac')[0].innerHTML = "AC";
-					debug.log('C change to AC:' + this.acShow);
-				} else {
-					// else toogle AC button to C
-						$('#ac')[0].innerHTML = "C";
-					debug.log('AC change to C:' + this.acShow);
-				}
+		if ( this.acShow ) {
+			// then toogle C button to AC
+			$('#ac')[0].innerHTML = "AC";
+			debug.log('C change to AC:' + this.acShow);
+
+		} else {
+			// else toogle AC button to C
+				$('#ac')[0].innerHTML = "C";
+			debug.log('AC change to C:' + this.acShow);
+
+		}
+
 	},
 
 	displayValue: function (value) {
-			$('.display').html(
-				'<input type="text" id="input1" class="input" placeholder=""></input><input type="text" id="inputNoBlink" class="input" value="' + value + '"></input><input type="text" id="input2" class="input" placeholder=""></input>');
+		$('.display').html('<input type="text" id="input1" class="input" placeholder=""></input><input type="text" id="inputNoBlink" class="input" value="' + value + '"></input><input type="text" id="input2" class="input" placeholder=""></input>');
+
 	},
 
 	displayVersion: function (version) {
@@ -183,90 +162,86 @@ if (debug.loggingOn) {console.log.bind(window.console)}
 
 // event-listeners
 
-	$('#ac').click(function(event){
+$('#ac').click(function(event){
 
-			allClear = $(this).text();
+	allClear = $(this).text();
 
+	if ( allClear == 'C' ) {
 
-			if ( allClear == 'C' ) {
+		debug.log('C clicked, roll back to last entry');
 
-				debug.log('C clicked, roll back to last entry');
+		if ( opPressed ) {
 
-				if ( opPressed ) {
+			 inputValue = '';
+			 opValue = '';
+			 debug.log('opValue:' + opValue);
+			 debug.log('opValue:' + numberValue);
+		} else if ( numberPressed ){
 
-					 inputValue = '';
-					 opValue = '';
-					 debug.log('opValue:' + opValue);
-					 debug.log('opValue:' + numberValue);
+			 inputValue = '';
+			 firstNumberValue = initialValue;
+			 numberValue = firstNumberValue;
+			 secondNumberValue = 0;
+			 debug.log('firstNumberValue:' + firstNumberValue);
+			 debug.log('secondNumberValue:' + secondNumberValue);
 
-				} else if ( numberPressed ){
+		} else { // if equals just pressed
 
-					 inputValue = '';
-					 firstNumberValue = initialValue;
-					 numberValue = firstNumberValue;
-					 secondNumberValue = 0;
-					 debug.log('firstNumberValue:' + firstNumberValue);
-					 debug.log('secondNumberValue:' + secondNumberValue);
+			calcSum = 0;
+			ux.displayValue(calcSum);
+			debug.log('calcSum:' + calcSum);
+			calcStatus = 0;
+			debug.log('calcStatus:' + calcStatus);
+			numberValue = '';
+			debug.log('numberValue:' + numberValue);
+			opValue = '';
+			debug.log('opValue:' + opValue);
+			equals = false;
+			debug.log('equals:' + equals);
+			numberPressed = false;
+			debug.log('numberPressed:' + numberPressed);
+			opPressed = false;
+			debug.log('opPressed:' + opPressed);
+			percent = false;
+			debug.log('percent:' + percent);
+			firstNumberValue = 0;
+			debug.log('firstNumberValue:' + firstNumberValue);
+			secondNumberValue = 0;
+			debug.log('secondNumberValue:' + secondNumberValue);
+		}
 
-				} else { // if equals just pressed
+		ux.acShow = true;
+		ux.acToogle();
 
-					calcSum = 0;
-					ux.displayValue(calcSum);
-					debug.log('calcSum:' + calcSum);
-					calcStatus = 0;
-					debug.log('calcStatus:' + calcStatus);
-					numberValue = '';
-					debug.log('numberValue:' + numberValue);
-					opValue = '';
-					debug.log('opValue:' + opValue);
-					equals = false;
-					debug.log('equals:' + equals);
-					numberPressed = false;
-					debug.log('numberPressed:' + numberPressed);
-					opPressed = false;
-					debug.log('opPressed:' + opPressed);
-					percent = false;
-					debug.log('percent:' + percent);
-					firstNumberValue = 0;
-					debug.log('firstNumberValue:' + firstNumberValue);
-					secondNumberValue = 0;
-					debug.log('secondNumberValue:' + secondNumberValue);
+	} else if ( allClear === 'AC') {
 
+		 debug.log('AC clicked, reseting all values');
 
-				}
+		 calcSum = 0;
+		 ux.displayValue(calcSum);
+		 debug.log('calcSum:' + calcSum);
+		 calcStatus = 0;
+		 debug.log('calcStatus:' + calcStatus);
+		 numberValue = '';
+		 debug.log('numberValue:' + numberValue);
+		 opValue = '';
+		 debug.log('opValue:' + opValue);
+		 equals = false;
+		 debug.log('equals:' + equals);
+		 numberPressed = false;
+		 debug.log('numberPressed:' + numberPressed);
+		 opPressed = false;
+		 debug.log('opPressed:' + opPressed);
+		 percent = false;
+		 debug.log('percent:' + percent);
+		 firstNumberValue = 0;
+		 debug.log('firstNumberValue:' + firstNumberValue);
+		 secondNumberValue = 0;
+		 debug.log('secondNumberValue:' + secondNumberValue);
+		 // TODO: storage of memory values cleared
+		 debug.log("memory storage cleared ")
 
-				ux.acShow = true;
-				ux.acToogle();
-
-			} else if ( allClear === 'AC') {
-
-				 debug.log('AC clicked, reseting all values');
-
-				 calcSum = 0;
-				 ux.displayValue(calcSum);
-				 debug.log('calcSum:' + calcSum);
-				 calcStatus = 0;
-				 debug.log('calcStatus:' + calcStatus);
-				 numberValue = '';
-				 debug.log('numberValue:' + numberValue);
-				 opValue = '';
-				 debug.log('opValue:' + opValue);
-				 equals = false;
-				 debug.log('equals:' + equals);
-				 numberPressed = false;
-				 debug.log('numberPressed:' + numberPressed);
-				 opPressed = false;
-				 debug.log('opPressed:' + opPressed);
-				 percent = false;
-				 debug.log('percent:' + percent);
-				 firstNumberValue = 0;
-				 debug.log('firstNumberValue:' + firstNumberValue);
-				 secondNumberValue = 0;
-				 debug.log('secondNumberValue:' + secondNumberValue);
-				 // TODO: storage of memory values cleared
-				 debug.log("memory storage cleared ")
-
-			 }
+	 }
 	}); // end all-clear button event handler
 
 	$('#posNeg, #percent').click(function(event){
@@ -348,6 +323,7 @@ if (debug.loggingOn) {console.log.bind(window.console)}
 
 
 		if (equals) {
+
 				numberValue = '';
 				equals = false;
 		 }
@@ -404,6 +380,7 @@ if (debug.loggingOn) {console.log.bind(window.console)}
 
 					// if just summed using equals
 					if (equals){
+
 						firstNumberValue = calcSum;
 					}
 
